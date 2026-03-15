348dd0c99d6b39f8fca5b2bd7aa2da8ebcd6ed3f
Alessandro Maurini <maurini.alessandro.23@itisriva.edu.it>
2026-03-15 14:56:46 +0100

    menu con matrici

---
+++ matrici_1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrici_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continua = true;
+            int[,] matrice = null;
+            while (continua)
+            {
+                stampaMenu();
+                Console.Write("Scegliere la prossima operazione: ");
+                string scelta = Console.ReadLine();
+                switch (scelta)
+                {
+                    case "1":
+                        Console.Write("Numero righe: ");
+                        int righe = int.Parse(Console.ReadLine());
+
+                        Console.Write("Numero colonne: ");
+                        int colonne = int.Parse(Console.ReadLine());
+
+                        matrice = new int[righe, colonne];
+
+                        for (int i = 0; i < righe; i++)
+                        {
+                            for (int j = 0; j < colonne; j++)
+                            {
+                                Console.Write($"Inserisci valore [{i},{j}]: ");
+                                matrice[i, j] = int.Parse(Console.ReadLine());
+                            }
+                        }
+                        break;
+
+                    case "2":
+                        for (int i = 0; i < matrice.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < matrice.GetLength(1); j++)
+                            {
+                                Console.Write(matrice[i, j] + "\t");
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+
+                    case "3":
+                        continua = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Scelta non valida.");
+                        break;
+                }
+            }
+        }
+        static void stampaMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("1 - Creazione matrice");
+            Console.WriteLine("2 - Stampa matrice");
+            Console.WriteLine("0 - ESCI");
+        }
+    }
+}
